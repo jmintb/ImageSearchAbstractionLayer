ddef10d577f2c2b049b6978b000c45eee0ca0aad
Jessie Chatham Spencer <isai@protonmail.com>
2017-06-17 13:06:54 +0000

    Added package.json

--- app/routing/routing.js
+++ app/routing/routing.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     var path = require('path');
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname+'index.html'));
+        res.sendFile(path.join(__dirname+'../usageGuide/index.html'));
     });
 }
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var mongo = require('mongodb').MongoClient;
-var imgsal = require('./app/imgsal.js');
+var imgsal = require('./app/service/imgsal.js');
 var express = require('express');
 var app = express();
-var guide = require('./guide/guide.js');
+var routing = require('./app/routing/routing.js');
 var port = process.env.PORT || 8080;
-var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/data';
 var apiKey = '5655684-7d917774fc6331a4a31c06cc7';
 
-app.use(express.static(__dirname + '/guide'))
-guide(app);
+app.use(express.static(__dirname + '/app/usageGuide'))
 
-mongo.connect(mongoUrl, function(err, db) {
-    if (err) throw err;
-
-    imgsal(app, db, apiKey);
+mongo.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/data', function(err, db) {
+    if (err){ 
+        throw err
+    }
+    imgsal(app, db, apiKey);    
 });
+
+routing(app);
 
 app.listen(port);
