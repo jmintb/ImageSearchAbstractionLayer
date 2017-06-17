26a93dfdc31abf8b32274007ae00781f4715f56e
Jessie Chatham Spencer <isai@protonmail.com>
2017-06-17 11:22:36 +0000

    Added guide page, explaining usage.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var mongo = require('mongodb').MongoClient;
 var imgsal = require('./app/imgsal.js');
-var app = require('express')();
+var express = require('express');
+var app = express();
+var guide = require('./guide/guide.js');
 var port = process.env.PORT || 8080;
 var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/data';
 var apiKey = '5655684-7d917774fc6331a4a31c06cc7';
+
+app.use(express.static(__dirname + '/guide'))
+guide(app);
 
 mongo.connect(mongoUrl, function(err, db) {
     if (err) throw err;
